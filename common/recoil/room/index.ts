f2ea24099b4cb808722596a7e31282bfd1dfabd6
shefali2004 <141213664+shefali2004@users.noreply.github.com>
2024-04-20 01:48:17 +0530

    Add files via upload

---
+++ common/recoil/room/index.ts
@@ -0,0 +1,7 @@
+
+import { roomAtom } from "./room.atom";
+import { useRoom, useSetRoomId, useSetUsers, useMyMoves } from "./room.hooks";
+
+export default roomAtom;
+
+export { useRoom, useSetRoomId, useSetUsers, useMyMoves };
---
+++ common/recoil/users/index.ts
@@ -0,0 +1,4 @@
+import { usersAtom } from "./users.atoms";
+import { useUserIds,useUsers } from "./users.hooks";
+export default usersAtom;
+export {useUserIds,useUsers};
